902cd939dd9dd24dc1fb175d75997ecf07c1251d
DHIVAKER29 <ssdddhivaker@gmail.com>
2024-08-08 23:12:12 +0530

    Coonection Pool of Database Implementation

---
+++ SingletonDesign Pattern/ConnectionPool/ConnectionPool.java
@@ -0,0 +1,16 @@
+package com.assignments.question;
+
+
+public interface ConnectionPool {
+
+    void initializePool();
+
+    DatabaseConnection getConnection();
+
+    void releaseConnection(DatabaseConnection connection);
+
+    int getAvailableConnectionsCount();
+
+    int getTotalConnectionsCount();
+
+}
---
+++ SingletonDesign Pattern/ConnectionPool/DataBaseConnection.java
@@ -0,0 +1,5 @@
+package com.assignments.question;
+
+public class DatabaseConnection {
+}
+
