966cc636c9e7d13b61fe2372298bbdc5a741d82e
DHIVAKER29 <ssdddhivaker@gmail.com>
2024-08-09 11:03:09 +0530

    Modified ConnectionPoolImpl file

--- SingletonDesign Pattern/ConnectionPool/ConnectionPoolImpl.java
+++ SingletonDesign Pattern/ConnectionPool/ConnectionPoolImpl.java
@@ -1,16 +1,72 @@
 package com.assignments.question;
 
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 
-public interface ConnectionPool {
+public class ConnectionPoolImpl implements ConnectionPool {
 
-    void initializePool();
+    private static ConnectionPool instance = null;
+    private final int maxConnections;
+    private final BlockingQueue<DatabaseConnection> connectionQueue;
 
-    DatabaseConnection getConnection();
+    private ConnectionPoolImpl(int maxConnections) {
+        this.maxConnections = maxConnections;
+        connectionQueue = new ArrayBlockingQueue<>(maxConnections);
+        initializePool();
+    }
 
-    void releaseConnection(DatabaseConnection connection);
+    public static ConnectionPool getInstance(int maxConnections) {
+        if (instance == null) {
+            instance = new ConnectionPoolImpl(maxConnections);
+        }
+        return instance;
+    }
 
-    int getAvailableConnectionsCount();
+    public static void resetInstance() {
+        instance = null;
+    }
 
-    int getTotalConnectionsCount();
+    @Override
+    public void initializePool() {
+        for (int i = 0; i < maxConnections; i++) {
+            DatabaseConnection connection = createConnection();
+            connectionQueue.offer(connection);
+        }
+    }
 
+    private DatabaseConnection createConnection() {
+        return new DatabaseConnection();
+    }
+
+    @Override
+    public DatabaseConnection getConnection() {
+        try {
+            return connectionQueue.take();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new RuntimeException("Error getting connection", e);
+        }
+    }
+
+    @Override
+    public void releaseConnection(DatabaseConnection connection) {
+        try {
+            if (connection != null) {
+                connectionQueue.offer(connection);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("Error releasing connection", e);
+        }
+    }
+
+    @Override
+    public int getAvailableConnectionsCount() {
+        return connectionQueue.size();
+    }
+
+    @Override
+    public int getTotalConnectionsCount() {
+        return maxConnections;
+    }
 }
+
